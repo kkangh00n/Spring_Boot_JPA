43cd3e33becfacde8da62bddb23b43d91f0136cf
kkangh00n <kig2454@gmail.com>
2023-06-01 23:22:09 +0900

    feat: 회원 등록 API

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.api;
+
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.dto.CreateMemberRequest;
+import jpabook.jpashop.domain.dto.CreateMemberResponse;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PostMapping()
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+}
---
+++ src/main/java/jpabook/jpashop/domain/dto/CreateMemberRequest.java
@@ -0,0 +1,8 @@
+package jpabook.jpashop.domain.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateMemberRequest {
+    private String name;
+}
