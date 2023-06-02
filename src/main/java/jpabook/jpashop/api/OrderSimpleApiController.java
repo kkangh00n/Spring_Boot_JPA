f914c2fab3d5fc9b592997a4eeaf92824cc4424c
kkangh00n <kig2454@gmail.com>
2023-06-02 19:20:20 +0900

    refactor: 엔티티 -> DTO 변환

---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.dto.SimpleOrderDto;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> orderV1(){
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> orderV2(){
+        List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream().map(SimpleOrderDto::new).collect(Collectors.toList());
+
+        return result;
+    }
+}
---
+++ src/main/java/jpabook/jpashop/domain/dto/SimpleOrderDto.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain.dto;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+public class SimpleOrderDto {
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+
+    public SimpleOrderDto(Order order){
+        this.orderId = order.getId();
+        this.name = order.getMember().getName();
+        this.orderDate = order.getOrderDate();
+        this.orderStatus = order.getStatus();
+        this.address = order.getDelivery().getAddress();
+    }
+}
