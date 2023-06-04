3763780ec2f4d897e001d877fd25d5267358ad7b
kkangh00n <kig2454@gmail.com>
2023-06-04 23:35:15 +0900

    feat: 컬렉션 조회 최적화 V3 (페치 조인 최적화)

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -43,4 +43,14 @@ public class OrderApiController {
 
         return result;
     }
+
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> orderV3(){
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> result = orders.stream()
+                .map(OrderDto::new)
+                .collect(Collectors.toList());
+
+        return result;
+    }
 }
--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -57,7 +57,7 @@ public class OrderRepository {
         return query.getResultList();
     }
 
-    public List<Order> findAllwithMemberDelivery(){
+    public List<Order> findAllWithMemberDelivery(){
         return em.createQuery("select o from Order o" +
                                         " join fetch o.member m" +
                                         " join fetch o.delivery d", Order.class)
@@ -71,4 +71,14 @@ public class OrderRepository {
                                         " join o.delivery d", OrderSimpleQueryDto.class)
                 .getResultList();
     }
+
+    public List<Order> findAllWithItem(){
+        return em.createQuery("select o from Order o" +
+                " join fetch o.member m" +
+                " join fetch o.delivery d" +
+                " join fetch o.orderItems oi" +
+                " join fetch oi.item i", Order.class)
+                .getResultList();
+    }
+
 }
